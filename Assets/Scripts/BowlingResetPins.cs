6cf252c0306961f4b14019d5063bbb13578aaa22
Carter Hinsley <kvazau@protonmail.com>
2018-07-04 19:47:48 -0400

    Add reset for bowling pins' position

---
+++ Assets/Scripts/BowlingResetPins.cs
@@ -0,0 +1,23 @@
+﻿/* Attach this script to the GameObject parent of bowling pin GameObjects. */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingResetPins : MonoBehaviour {
+    public GameObject bowlingPinPrefab;
+    private GameObject bowlingPins;
+    
+	// Use this for initialization
+	void Start () {
+        bowlingPins = Instantiate(bowlingPinPrefab);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.GetDown(OVRInput.Button.One)) {
+            Destroy(bowlingPins);
+            bowlingPins = Instantiate(bowlingPinPrefab);
+        }
+	}
+}
